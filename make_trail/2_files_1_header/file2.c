e1140af6c100843369a790aa5d012796118eb0fd
vividdesi <rahulnani988@gmail.com>
2024-11-20 14:09:37 +0530

    Repo:makefiles asighnments
    Signed-off-by: vividdesi <rahulnani988@gmail.com>

---
+++ make_trail/2_files_1_header/file2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"header_file.h"
+int main()
+{
+        int a=30,b=40;
+        add(a,b);
+        sub(a,b);
+//        div(a,b);
+        return 0;
+}
+
---
+++ make_trail/2_files_1_header/header_file.h
@@ -0,0 +1,14 @@
+// header_file.h
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+static inline int add(int a, int b) {
+    return a + b;
+}
+
+static inline int sub(int a, int b) {
+    return a - b;
+}
+
+#endif
+
